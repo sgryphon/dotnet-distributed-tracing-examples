142bbaf1b7f8ab15d7ed98910ee8969eb48589c4
Sly Gryphon <sly@gamertheory.net>
2021-09-24 16:18:30 +1000

    Service changes

--- src/1-basic/Demo.Service/Controllers/WeatherForecastController.cs
+++ src/1-basic/Demo.Service/Controllers/WeatherForecastController.cs
@@ -26,6 +26,8 @@ namespace Demo.Service.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+            _logger.LogInformation(2100, "Service weather forecast requested");
+
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
