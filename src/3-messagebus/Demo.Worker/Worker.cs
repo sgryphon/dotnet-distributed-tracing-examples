2427bdf1ebdced22adea0d0172847b8015365ee2
Sly Gryphon <sly@gamertheory.net>
2021-09-27 23:14:09 +1000

    Add message receiver to worker

--- src/3-messagebus/Demo.Worker/Worker.cs
+++ src/3-messagebus/Demo.Worker/Worker.cs
@@ -11,17 +11,32 @@ namespace Demo.Worker
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly Azure.Messaging.ServiceBus.ServiceBusClient _serviceBusClient;
 
-        public Worker(ILogger<Worker> logger)
+        public Worker(ILogger<Worker> logger, Azure.Messaging.ServiceBus.ServiceBusClient serviceBusClient)
         {
             _logger = logger;
+            _serviceBusClient = serviceBusClient;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            await using var serviceBusProcessor = _serviceBusClient.CreateProcessor("demo-queue");
+            serviceBusProcessor.ProcessMessageAsync += args =>
+            {
+                _logger.LogInformation(2003, "Message received: {MessageBody}", args.Message.Body);
+                return Task.CompletedTask;
+            };
+            serviceBusProcessor.ProcessErrorAsync += args =>
+            {
+                _logger.LogError(5000, args.Exception, "Service bus error");
+                return Task.CompletedTask;
+            };
+            await serviceBusProcessor.StartProcessingAsync(stoppingToken);
+
             while (!stoppingToken.IsCancellationRequested)
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                //_logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
                 await Task.Delay(1000, stoppingToken);
             }
         }
