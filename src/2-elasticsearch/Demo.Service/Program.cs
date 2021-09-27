701fcd09b1de402a954f5f718f54773ca11fcfbf
Sly Gryphon <sly@gamertheory.net>
2021-09-27 14:36:43 +1000

    Add reference to Elasticsearch

--- src/2-elasticsearch/Demo.Service/Program.cs
+++ src/2-elasticsearch/Demo.Service/Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Elasticsearch.Extensions.Logging;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -18,6 +19,10 @@ namespace Demo.Service
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureLogging((hostContext, loggingBuilder) =>
+                {
+                    loggingBuilder.AddElasticsearch();
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
