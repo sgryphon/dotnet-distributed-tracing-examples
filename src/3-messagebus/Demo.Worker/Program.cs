473bc20daf05a30aef41fb4c1898b899ac87189a
Sly Gryphon <sly@gamertheory.net>
2021-09-27 22:29:42 +1000

    Add Elasticsearch logging to worker demo

--- src/3-messagebus/Demo.Worker/Program.cs
+++ src/3-messagebus/Demo.Worker/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Elasticsearch.Extensions.Logging;
 
 namespace Demo.Worker
 {
@@ -16,6 +17,10 @@ namespace Demo.Worker
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureLogging((hostContext, loggingBuilder) =>
+                {
+                    loggingBuilder.AddElasticsearch();
+                })
                 .ConfigureServices((hostContext, services) =>
                 {
                     services.AddHostedService<Worker>();
