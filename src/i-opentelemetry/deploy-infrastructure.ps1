91514790cf33f5ff0c92257359e9f8729075c1ae
Sly Gryphon <sly@gamertheory.net>
2022-01-19 18:47:17 +1000

    Clean up ready for OTel

--- src/i-opentelemetry/deploy-infrastructure.ps1
+++ src/i-opentelemetry/deploy-infrastructure.ps1
@@ -1,69 +1,55 @@
 #!/usr/bin/env pwsh
 
 <# .SYNOPSIS
-  Deploy the Azure infrastructure. #>
-  [CmdletBinding()]
-  param (
-      ## Identifier for the organisation or subscription to make global names unique.
-      [string]$OrgId = "0x$($(az account show --query id --output tsv).Substring(0,4))",
-      ## Deployment environment, e.g. Prod, Dev, QA, Stage, Test.
-      [string]$Environment = 'Dev',
-      ## The Azure region where the resource is deployed.
-      [string]$Location = 'australiaeast'
-  )
+  Deploy the Azure infrastructure.
 
-# Pre-requisites:
+.NOTES
 
-# Running these scripts requires the following to be installed:
-#  * PowerShell, https://github.com/PowerShell/PowerShell
-#  * Azure CLI, https://docs.microsoft.com/en-us/cli/azure/
-#
-# To run:
-#   az login
-#   az account set --subscription <subscription id>
-#   $VerbosePreference = 'Continue'
-#   ./deploy-infrastructure.ps1
+  Running these scripts requires the following to be installed:
+  * PowerShell, https://github.com/PowerShell/PowerShell
+  * Azure CLI, https://docs.microsoft.com/en-us/cli/azure/
+
+  You also need to connect to Azure (log in), and set the desired subscription context.
+
+  Follow standard naming conventions from Azure Cloud Adoption Framework, 
+  with an additional organisation or subscription identifier (after app name) in global names 
+  to make them unique.
+  https://docs.microsoft.com/en-us/azure/cloud-adoption-framework/ready/azure-best-practices/resource-naming
+
+  Follow standard tagging conventions from  Azure Cloud Adoption Framework.
+  https://docs.microsoft.com/en-us/azure/cloud-adoption-framework/ready/azure-best-practices/resource-tagging
+
+.EXAMPLE
+
+   az login
+   az account set --subscription <subscription id>
+   $VerbosePreference = 'Continue'
+   ./deploy-infrastructure.ps1
+#>
+[CmdletBinding()]
+param (
+    ## Number of initial scripts to skip (if they have already been run)
+    [int]$Skip = 0,
+    ## Deployment environment, e.g. Prod, Dev, QA, Stage, Test.
+    [string]$Environment = $ENV:DEPLOY_ENVIRONMENT ?? 'Dev',
+    ## The Azure region where the resource is deployed.
+    [string]$Location = $ENV:DEPLOY_LOCATION ?? 'australiaeast',
+    ## Identifier for the organisation (or subscription) to make global names unique.
+    [string]$OrgId = $ENV:DEPLOY_ORGID ?? "0x$((az account show --query id --output tsv).Substring(0,4))"
+)
 
 $ErrorActionPreference="Stop"
 
 $SubscriptionId = $(az account show --query id --output tsv)
-Write-Verbose "Using context subscription ID $SubscriptionId"
+Write-Verbose "Deploying scripts for environment '$Environment' in subscription '$SubscriptionId'"
 
-# Following standard naming conventions from Azure Cloud Adoption Framework
-# https://docs.microsoft.com/en-us/azure/cloud-adoption-framework/ready/azure-best-practices/resource-naming
-# With an additional organisation or subscription identifier (after app name) in global names to make them unique 
+$scriptItems = Get-ChildItem "$PSScriptRoot/infrastructure" -Filter '*.ps1' `
+  | Sort-Object -Property Name `
+  | Select-Object -Skip $Skip
 
-$appName = 'tracedemo'
+$scriptItems | ForEach-Object { 
+  Write-Verbose "Running $($_.Name)"
+  & $_.FullName -Environment $Environment -Location $Location -OrgId $OrgId
+}
 
-$rgName = "rg-$appName-$Environment-001".ToLowerInvariant()
-$sbName = "sb-$appName-$OrgId-$Environment".ToLowerInvariant()
-$sbqName = "sbq-demo".ToLowerInvariant()
-
-# Following standard tagging conventions from  Azure Cloud Adoption Framework
-# https://docs.microsoft.com/en-us/azure/cloud-adoption-framework/ready/azure-best-practices/resource-tagging
-
-$TagDictionary = @{ WorkloadName = 'tracingdemo'; DataClassification = 'Non-business'; Criticality = 'Low';
-  BusinessUnit = 'Demo'; ApplicationName = $appName; Env = $Environment }
-
-# Create
-
-Write-Host "Creating group $rgName"
-
-# Convert dictionary to tags format used by Azure CLI create command
-$tags = $TagDictionary.Keys | ForEach-Object { $key = $_; "$key=$($TagDictionary[$key])" }
-az group create -g $rgName -l $location --tags $tags
-
-Write-Host "Creating service bus $sbName with queue $sbqName"
-
-# Convert tags returned from JSON result to the format used by Azure CLI create command
-$rg = az group show --name $rgName | ConvertFrom-Json
-$rgTags = $rg.tags | Get-Member -MemberType NoteProperty | ForEach-Object { "$($_.Name)=$($rg.tags.$($_.Name))" }
-
-az servicebus namespace create --name $sbName --resource-group $rgName -l $rg.location --sku Standard --tags $rgTags
-az servicebus queue create --name $sbqName --namespace-name $sbName --resource-group $rgName
-
-# Output
-
-Write-Host "Connection string"
-
-az servicebus namespace authorization-rule keys list --namespace-name $sbName --resource-group $rgName --name RootManageSharedAccessKey --query primaryConnectionString -o tsv
+Write-Verbose "Deployment Complete"
