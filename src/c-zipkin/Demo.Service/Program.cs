8ad34324f1e6edc3992582af92fe07528ea20fa6
Sly Gryphon <sly@gamertheory.net>
2022-01-26 06:46:42 +1000

    Copy of base otel for Zipkin example

---
+++ src/c-zipkin/Demo.Service/Program.cs
@@ -0,0 +1,54 @@
+using OpenTelemetry.Logs;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Configure OpenTelemetry service resource details
+var entryAssembly = System.Reflection.Assembly.GetEntryAssembly();
+var entryAssemblyName = entryAssembly?.GetName();
+var versionAttribute = entryAssembly?.GetCustomAttributes(false)
+    .OfType<System.Reflection.AssemblyInformationalVersionAttribute>()
+    .FirstOrDefault();
+var resourceBuilder = ResourceBuilder.CreateDefault().AddService(entryAssemblyName?.Name,
+    serviceVersion: versionAttribute?.InformationalVersion ?? entryAssemblyName?.Version?.ToString());
+
+// Configure logging
+builder.Logging.ClearProviders()
+    .AddOpenTelemetry(configure =>
+    {
+        configure
+            .SetResourceBuilder(resourceBuilder)
+            .AddConsoleExporter();
+    });
+
+// Add services to the container.
+builder.Services.AddOpenTelemetryTracing(configure =>
+{
+    configure
+        .SetResourceBuilder(resourceBuilder)
+        .AddAspNetCoreInstrumentation()
+        .AddConsoleExporter();
+});
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
