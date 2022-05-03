9fa95d3a82dd45bc4657035173a6f1d6f393abdb
Sly Gryphon <sly.gryphon@purple.telstra.com>
2022-05-03 17:24:18 +1000

    Add new worker project

---
+++ src/c-complex/Demo.Worker/Program.cs
@@ -0,0 +1,10 @@
+using Demo.Worker;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
