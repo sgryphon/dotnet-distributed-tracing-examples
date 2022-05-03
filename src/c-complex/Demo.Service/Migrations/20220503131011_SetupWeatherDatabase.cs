604a6eeba5c4942bb07573782cfd70a3f9ea521f
Sly Gryphon <sly.gryphon@purple.telstra.com>
2022-05-03 23:10:52 +1000

    Initial migration

---
+++ src/c-complex/Demo.Service/Migrations/20220503131011_SetupWeatherDatabase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Demo.Service.Migrations
+{
+    public partial class SetupWeatherDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WeatherServiceRequests",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Note = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WeatherServiceRequests", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WeatherServiceRequests");
+        }
+    }
+}
