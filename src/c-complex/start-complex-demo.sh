adeff1f757e9fc35f7465223058113a4340a9cea
Sly Gryphon <sly.gryphon@purple.telstra.com>
2022-05-09 15:38:56 +1000

    Set version when running demo

--- src/c-complex/start-complex-demo.sh
+++ src/c-complex/start-complex-demo.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+version=$(dotnet gitversion /output json /showvariable InformationalVersion)
 tmux new-session -d 'ASPNETCORE_URLS="http://localhost:8002" npm run start --prefix Demo.WebApp/ClientApp'
-tmux split-window -h 'dotnet run --project Demo.WebApp --urls "http://*:8002" --environment Development'
-tmux split-window -f 'dotnet run --project Demo.Service --urls "https://*:44301" --environment Development'
-tmux split-window -h 'dotnet run --project Demo.Worker --environment Development'
+tmux split-window -h "dotnet run --project Demo.WebApp -p:InformationalVersion=$version -- --urls http://*:8002 --environment Development"
+tmux split-window -f "dotnet run --project Demo.Service -p:InformationalVersion=$version -- --urls https://*:44301 --environment Development"
+tmux split-window -h "dotnet run --project Demo.Worker -p:InformationalVersion=$version -- --environment Development"
 tmux attach-session -d
